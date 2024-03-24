6fffaff46a4b8a94f7090843368c13850e4c515f
kadirbekmn <kadir.bekman@gmail.com>
2024-03-24 17:39:47 +0300

    kadirprdeneme1

--- MyBookingApp4/src/Main.java
+++ MyBookingApp4/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("pr deneme hasan");
+		System.out.println("pr deneme kadir");
+
 	}
 
 }
